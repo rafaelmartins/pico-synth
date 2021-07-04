eb88c392863a12a0bf2dd032e129929128b31640
Rafael G. Martins <rafael@rafaelmartins.eng.br>
2021-07-04 23:48:38 +0200

    added eeprom-i2c (24LC256) library. not used yet

---
+++ eeprom-i2c/include/eeprom-i2c.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <hardware/i2c.h>
+
+typedef struct {
+    i2c_inst_t *i2c;
+    uint8_t i2c_address;
+    enum {
+        EEPROM_I2C_24LC256 = 1,
+    } ic;
+
+    // private
+    size_t page_size;
+} eeprom_i2c_t;
+
+int eeprom_i2c_init(eeprom_i2c_t *m);
+int eeprom_i2c_read(eeprom_i2c_t *m, uint16_t addr, uint8_t *data, size_t data_len);
+int eeprom_i2c_write(eeprom_i2c_t *m, uint16_t addr, uint8_t *data, size_t data_len);
